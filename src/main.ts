0c040f4186bca01c49c1d7d0cfae4372d539c07a
xinghaixizhi <344401468@qq.com>
2020-12-22 19:26:16 +0800

    引入vue-router和vuex

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import installElementPlus from './plugins/element'
+import router from './router'
 import '../node_modules/normalize.css'
+import store from './store'
+
 
 const app = createApp(App)
+app.use(store)
+app.use(router)
 // 按需加载element组件
 installElementPlus(app)
 app.mount('#app')
